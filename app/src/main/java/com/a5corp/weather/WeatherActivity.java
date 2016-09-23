21abe7671269dc33268904e64de332de1729d89a
Aaditya Menon <AadityaMenon007@gmail.com>
2016-09-23 12:26:06 +0530

    Beautifying the Change City Editor

--- app/src/main/java/com/a5corp/weather/WeatherActivity.java
+++ app/src/main/java/com/a5corp/weather/WeatherActivity.java
@@ -8,8 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.InputType;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+
+import static java.security.AccessController.getContext;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -44,9 +50,9 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     private void showInputDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Change city");
-        final AppCompatEditText input = new AppCompatEditText(this);
+        final EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         builder.setView(input);
         builder.setPositiveButton("Go", new DialogInterface.OnClickListener() {
@@ -55,7 +61,32 @@ public class WeatherActivity extends AppCompatActivity {
                 changeCity(input.getText().toString());
             }
         });
-        builder.show();
+        builder.show();*/
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+        input.setSingleLine();
+        FrameLayout container = new FrameLayout(this);
+        FrameLayout.LayoutParams params = new  FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin= convertDpToPx(20); // remember to scale correctly
+        params.rightMargin= convertDpToPx(30);
+        input.setLayoutParams(params);
+        container.addView(input);
+        alert.setTitle("Change City");
+        alert.setMessage("You can change the city here");
+        alert.setView(container);
+        alert.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeCity(input.getText().toString());
+            }
+        });
+        alert.show();
+    }
+
+    public int convertDpToPx(int dp) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+        return px;
     }
 
     public void changeCity(String city){
