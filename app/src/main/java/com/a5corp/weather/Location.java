7e55479cfa97096b3d86f2f8bc600a5c05a46b50
Aaditya Menon <AadityaMenon007@gmail.com>
2016-10-24 23:34:02 +0530

    Initializing Location Receiver

---
+++ app/src/main/java/com/a5corp/weather/Location.java
@@ -0,0 +1,12 @@
+package com.a5corp.weather;
+
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+public class Location {
+    int Latitude;
+    int Longitude;
+
+    private LocationManager locationMangaer=null;
+    private LocationListener locationListener=null;
+}
