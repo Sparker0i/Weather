07374ef902ab59e22b62813f6bafba11d44a0b79
Aaditya Menon <AadityaMenon007@gmail.com>
2016-10-25 00:14:51 +0530

    Bringing back Scroll Aware Behavior

---
+++ app/src/main/java/com/a5corp/weather/ScrollAwareFABBehavior.java
@@ -0,0 +1,32 @@
+package com.a5corp.weather;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ScrollAwareFABBehavior extends CoordinatorLayout.Behavior<FloatingActionButton>  {
+    public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout, final FloatingActionButton child,
+                                       final View directTargetChild, final View target, final int nestedScrollAxes) {
+        return true;
+    }
+
+    @Override
+    public void onNestedScroll(final CoordinatorLayout coordinatorLayout,
+                               final FloatingActionButton child,
+                               final View target, final int dxConsumed, final int dyConsumed,
+                               final int dxUnconsumed, final int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
