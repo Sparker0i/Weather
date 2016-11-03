13630f3db58f77306920e2b52d8c8802c5fd9c51
Aaditya Menon <AadityaMenon007@gmail.com>
2016-11-04 03:52:26 +0530

    Changed Navbar Colors

--- app/src/main/java/com/a5corp/weather/NewActivity.java
+++ app/src/main/java/com/a5corp/weather/NewActivity.java
@@ -2,6 +2,8 @@ package com.a5corp.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
@@ -15,6 +17,12 @@ public class NewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
         Card card = new Card(this);
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+                Toast.makeText(getApplicationContext(),"Clickable card", Toast.LENGTH_LONG).show();
+            }
+        });
         CardHeader header = new CardHeader(this , R.layout.carddemo_inner_content);
         card.addCardHeader(header);
         card.setTitle("Hello World");
