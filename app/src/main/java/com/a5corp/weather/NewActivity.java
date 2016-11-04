ce7fef00502927f07a6f1ea38038ade6a6e03e98
Aaditya Menon <AadityaMenon007@gmail.com>
2016-11-04 16:40:34 +0530

    First Working CardListView

--- app/src/main/java/com/a5corp/weather/NewActivity.java
+++ app/src/main/java/com/a5corp/weather/NewActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
 import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.view.CardListView;
 import it.gmariotti.cardslib.library.view.CardView;
 import it.gmariotti.cardslib.library.view.CardViewNative;
 
@@ -16,17 +20,27 @@ public class NewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        //Create a Card
         Card card = new Card(this);
-        card.setOnClickListener(new Card.OnCardClickListener() {
-            @Override
-            public void onClick(Card card, View view) {
-                Toast.makeText(getApplicationContext(),"Clickable card", Toast.LENGTH_LONG).show();
-            }
-        });
-        CardHeader header = new CardHeader(this , R.layout.carddemo_inner_content);
-        card.addCardHeader(header);
         card.setTitle("Hello World");
-        CardViewNative cardView = (CardViewNative) findViewById(R.id.cards_activity);
-        cardView.setCard(card);
+        card.setCardElevation(5);
+        card.setShadow(true);
+
+        //Create a CardHeader
+        CardHeader header = new CardHeader(this);
+
+        //Add Header to card
+        card.addCardHeader(header);
+        for (int i = 0; i < 10; ++i)
+            cards.add(card);
+
+        CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this,cards);
+
+        CardListView listView = (CardListView) findViewById(R.id.myList);
+        if (listView!=null){
+            listView.setAdapter(mCardArrayAdapter);
+        }
     }
 }
