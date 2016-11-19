d837f6b63ede50ca9e6dff90b97618402ebc1908
Aaditya Menon <AadityaMenon007@gmail.com>
2016-11-20 03:13:50 +0530

    Trying all possible combinations with the ActionBar

--- app/src/main/java/com/a5corp/weather/ParallaxActivity.java
+++ app/src/main/java/com/a5corp/weather/ParallaxActivity.java
@@ -17,6 +17,7 @@ public class ParallaxActivity extends AppCompatActivity implements ObservableScr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parallax);
+        ActionBar ab = getSupportActionBar();
 
         ObservableListView listView = (ObservableListView) findViewById(R.id.list);
         listView.setScrollViewCallbacks(this);
