c5ab2073b56cc7c8cd6e899604ae7c348648a1f9
Aaditya Menon <AadityaMenon007@gmail.com>
2016-11-20 01:36:47 +0530

    Basic Parallax Working

---
+++ app/src/main/java/com/a5corp/weather/ParallaxActivity.java
@@ -0,0 +1,54 @@
+package com.a5corp.weather;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
+import java.util.ArrayList;
+
+public class ParallaxActivity extends AppCompatActivity implements ObservableScrollViewCallbacks{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parallax);
+
+        ObservableListView listView = (ObservableListView) findViewById(R.id.list);
+        listView.setScrollViewCallbacks(this);
+
+        ArrayList<String> items = new ArrayList<String>();
+        for (int i = 1; i <= 100; i++) {
+            items.add("Item " + i);
+        }
+        listView.setAdapter(new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, items));
+    }
+
+    @Override
+    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+        ActionBar ab = getSupportActionBar();
+        if (scrollState == ScrollState.UP) {
+            if (ab.isShowing()) {
+                ab.hide();
+            }
+        } else if (scrollState == ScrollState.DOWN) {
+            if (!ab.isShowing()) {
+                ab.show();
+            }
+        }
+    }
+
+    @Override
+    public void onScrollChanged(int scrollY, boolean firstScroll,
+                                boolean dragging) {
+    }
+
+    @Override
+    public void onDownMotionEvent() {
+    }
+}
