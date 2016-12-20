50f82b2995ae9842447ec4a35a54db78c226e2d1
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-21 01:17:11 +0530

    Adding an improved Detail Activity

---
+++ app/src/main/java/com/a5corp/weather/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.a5corp.weather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
