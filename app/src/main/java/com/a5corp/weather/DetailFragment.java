0201a50b3c836be9649fe68cc72d0fcf81e41d5c
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-21 21:19:51 +0530

    Done with Top Layer

--- app/src/main/java/com/a5corp/weather/DetailFragment.java
+++ app/src/main/java/com/a5corp/weather/DetailFragment.java
@@ -1,24 +1,156 @@
 package com.a5corp.weather;
 
+import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DetailFragment extends Fragment {
 
     Typeface weatherFont;
     TextView cityField;
-    TextView dateText , tempText, maxText , minText, humidityText, sunriseText, sunsetText, speedText, pressureText;
+    TextView dateText , tempText, dayText , nightText, humidityText, sunriseText, sunsetText, speedText, pressureText;
     TextView weatherIcon, humidityIcon, sunriseIcon, sunsetIcon, speedIcon, pressureIcon;
+    JSONObject obj;
+    Intent intent;
 
     public DetailFragment() {
+    }
+
+    public void updateView() {
+        try {
+            obj = new JSONObject(intent.getStringExtra("jsonStr"));
+            setWeatherIcon();
+            humidityIcon.setText(getActivity().getString(R.string.humidity_icon));
+            sunsetIcon.setText(getActivity().getString(R.string.sunset_icon));
+            sunriseIcon.setText(getActivity().getString(R.string.sunrise_icon));
+            speedIcon.setText(getActivity().getString(R.string.speed_icon));
+            pressureIcon.setText(getActivity().getString(R.string.pressure_icon));
+
+            Long date1 = obj.getLong("dt");
+            Date expiry = new Date(Long.parseLong(Long.toString(date1)) * 1000);
+            String date = new SimpleDateFormat("EE, dd" , Locale.US).format(expiry);
+            dateText.setText(date);
+
+            long dy = obj.getJSONObject("main").getLong("day");
+            int day = (int) dy;
+            dayText.setText("Day : " + day);
+
+            long nt = obj.getJSONObject("main").getLong("night");
+            int night = (int) nt;
+            nightText.setText("Night : " + night);
+
+            SpannableString ss1=  new SpannableString(date + "\n"
+                    + obj.getJSONObject("temp").getLong("max") + "°" + "      "
+                    + obj.getJSONObject("temp").getLong("min") + "°" + "\n");
+            ss1.setSpan(new RelativeSizeSpan(1.1f), 0,7, 0); // set size
+            ss1.setSpan(new RelativeSizeSpan(1.4f) , 8 , 11 , 0);
+            tempText.setText(ss1);
+        }
+        catch (JSONException ex) {
+            Log.e("Detail View" , "Cannot Find Detail");
+            getActivity().finish();
+        }
+    }
+
+    private void setWeatherIcon() throws JSONException {
+        int id = obj.getJSONArray("weather").getJSONObject(0).getInt("id");
+        String icon = "";
+        switch(id) {
+            case 501 :
+            case 521 :
+            case 500 : icon = getActivity().getString(R.string.weather_drizzle);
+                break;
+            case 502 :
+            case 503 :
+            case 504 : icon = getActivity().getString(R.string.weather_rainy);
+                break;
+            case 511 : icon = getActivity().getString(R.string.weather_rain_wind);
+                break;
+            case 301 :
+            case 310 :
+            case 311 :
+            case 300 :
+            case 520 : icon = getActivity().getString(R.string.weather_shower_rain);
+                break;
+            case 522 :
+            case 531 :
+            case 200 :
+            case 201 :
+            case 202 :
+            case 210 :
+            case 211 :
+            case 212 :
+            case 221 :
+            case 230 :
+            case 231 :
+            case 232 : icon = getActivity().getString(R.string.weather_thunder);
+                break;
+            case 302 :
+            case 314 :
+            case 321 :
+            case 312 : icon = getActivity().getString(R.string.weather_heavy_drizzle);
+                break;
+            case 313 : icon = getActivity().getString(R.string.weather_rain_drizzle);
+                break;
+            case 612 :
+            case 602 : icon = getActivity().getString(R.string.weather_heavy_snow);
+                break;
+            case 611 : icon = getActivity().getString(R.string.weather_sleet);
+                break;
+            case 903 :
+            case 600 :
+            case 601 :
+            case 615 :
+            case 616 :
+            case 620 :
+            case 621 :
+            case 622 : icon = getActivity().getString(R.string.weather_snowy);
+                break;
+            case 701 :
+            case 702 :
+            case 721 : icon = getActivity().getString(R.string.weather_smoke);
+                break;
+            case 751 :
+            case 761 :
+            case 731 : icon = getActivity().getString(R.string.weather_dust);
+                break;
+            case 741 : icon = getActivity().getString(R.string.weather_foggy);
+                break;
+            case 762 : icon = getActivity().getString(R.string.weather_volcano);
+                break;
+            case 771 :
+            case 900 :
+            case 781 : icon = getActivity().getString(R.string.weather_tornado);
+                break;
+            case 904 :
+            case 800 : icon = getActivity().getString(R.string.weather_sunny);
+                break;
+            case 801 :
+            case 802 :
+            case 803 :
+            case 804 : icon = getActivity().getString(R.string.weather_cloudy);
+                break;
+            case 901 : icon = getActivity().getString(R.string.weather_storm);
+                break;
+            case 902 : icon = getActivity().getString(R.string.weather_hurricane);
+                break;
+        }
+        weatherIcon.setText(icon);
     }
 
     @Override
@@ -27,22 +159,28 @@ public class DetailFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         cityField = (TextView) rootView.findViewById(R.id.city_field);
         dateText = (TextView) rootView.findViewById(R.id.date_text);
-        maxText = (TextView) rootView.findViewById(R.id.max_text);
-        minText = (TextView) rootView.findViewById(R.id.min_text);
+        dayText = (TextView) rootView.findViewById(R.id.day_text);
+        nightText = (TextView) rootView.findViewById(R.id.min_text);
 
         weatherIcon = (TextView) rootView.findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
         tempText = (TextView) rootView.findViewById(R.id.temp_text);
 
         humidityIcon = (TextView) rootView.findViewById(R.id.humidity_icon);
+        humidityIcon.setTypeface(weatherFont);
         humidityText = (TextView) rootView.findViewById(R.id.humidity_text);
         speedIcon = (TextView) rootView.findViewById(R.id.speed_icon);
+        speedIcon.setTypeface(weatherFont);
         speedText = (TextView) rootView.findViewById(R.id.speed_text);
         pressureIcon = (TextView) rootView.findViewById(R.id.pressure_icon);
+        pressureIcon.setTypeface(weatherFont);
         pressureText = (TextView) rootView.findViewById(R.id.pressure_text);
 
         sunriseIcon = (TextView) rootView.findViewById(R.id.sunrise_icon);
+        sunriseIcon.setTypeface(weatherFont);
         sunriseText = (TextView) rootView.findViewById(R.id.sunrise_text);
         sunsetIcon = (TextView) rootView.findViewById(R.id.sunset_icon);
+        sunsetIcon.setTypeface(weatherFont);
         sunsetText = (TextView) rootView.findViewById(R.id.sunset_text);
 
         return rootView;
@@ -51,6 +189,8 @@ public class DetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/newweather.ttf");
+        intent = getActivity().getIntent();
+        updateView();
     }
 }
