588d41546443a478170abaa6d7f7615c29d2e9df
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-21 16:37:38 +0530

    Layout Ready
    Removed Unnecessary Drawables

--- app/src/main/java/com/a5corp/weather/DetailFragment.java
+++ app/src/main/java/com/a5corp/weather/DetailFragment.java
@@ -1,15 +1,22 @@
 package com.a5corp.weather;
 
+import android.graphics.Typeface;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class DetailFragment extends Fragment {
+
+    Typeface weatherFont;
+    TextView cityField;
+    TextView dateText , tempText, maxText , minText, humidityText, sunriseText, sunsetText, speedText, pressureText;
+    TextView weatherIcon, humidityIcon, sunriseIcon, sunsetIcon, speedIcon, pressureIcon;
 
     public DetailFragment() {
     }
@@ -17,6 +24,8 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        
+        return rootView;
     }
 }
