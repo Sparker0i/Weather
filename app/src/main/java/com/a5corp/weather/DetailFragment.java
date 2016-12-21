87fe40e60e8751d39760c73ffcb421db2a51f0f6
Aaditya Menon <AadityaMenon007@gmail.com>
2016-12-21 16:48:24 +0530

    Mapping TextViews to their XMLs

--- app/src/main/java/com/a5corp/weather/DetailFragment.java
+++ app/src/main/java/com/a5corp/weather/DetailFragment.java
@@ -25,7 +25,32 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        
+        cityField = (TextView) rootView.findViewById(R.id.city_field);
+        dateText = (TextView) rootView.findViewById(R.id.date_text);
+        maxText = (TextView) rootView.findViewById(R.id.max_text);
+        minText = (TextView) rootView.findViewById(R.id.min_text);
+
+        weatherIcon = (TextView) rootView.findViewById(R.id.weather_icon);
+        tempText = (TextView) rootView.findViewById(R.id.temp_text);
+
+        humidityIcon = (TextView) rootView.findViewById(R.id.humidity_icon);
+        humidityText = (TextView) rootView.findViewById(R.id.humidity_text);
+        speedIcon = (TextView) rootView.findViewById(R.id.speed_icon);
+        speedText = (TextView) rootView.findViewById(R.id.speed_text);
+        pressureIcon = (TextView) rootView.findViewById(R.id.pressure_icon);
+        pressureText = (TextView) rootView.findViewById(R.id.pressure_text);
+
+        sunriseIcon = (TextView) rootView.findViewById(R.id.sunrise_icon);
+        sunriseText = (TextView) rootView.findViewById(R.id.sunrise_text);
+        sunsetIcon = (TextView) rootView.findViewById(R.id.sunset_icon);
+        sunsetText = (TextView) rootView.findViewById(R.id.sunset_text);
+
         return rootView;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
     }
 }
