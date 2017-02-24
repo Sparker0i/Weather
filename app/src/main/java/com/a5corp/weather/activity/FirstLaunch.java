0225cb533414beecfde2716cd8ee6b228c66cbaa
Aaditya Menon <AadityaMenon007@gmail.com>
2017-02-24 23:16:56 +0530

    Removal of unnecessary code

--- app/src/main/java/com/a5corp/weather/activity/FirstLaunch.java
+++ app/src/main/java/com/a5corp/weather/activity/FirstLaunch.java
@@ -17,15 +17,5 @@ public class FirstLaunch extends AppCompatActivity {
         setContentView(R.layout.activity_first_launch);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
