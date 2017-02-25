97bc83d55390c52923568502368505aa8f8b6214
Aaditya Menon <AadityaMenon007@gmail.com>
2017-02-25 16:43:40 +0530

    Removing the Activity Superclass from Permissions

--- app/src/main/java/com/a5corp/weather/permissions/Permissions.java
+++ app/src/main/java/com/a5corp/weather/permissions/Permissions.java
@@ -12,7 +12,7 @@ import android.support.v4.app.ActivityCompat;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 
-public class Permissions extends Activity {
+public class Permissions {
     private Context mContext;
     final int READ_COARSE_LOCATION = 20;
 
@@ -26,7 +26,7 @@ public class Permissions extends Activity {
 
     public void showMaterialDialog() {
         MaterialDialog dialog;
-        MaterialDialog.Builder builder = new MaterialDialog.Builder(this);
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(mContext);
         builder.title("Permission needed")
                 .content("This Action Requires the Location Setting to be enabled. Go to Settings and check the Location Permission inside the Permissions View")
                 .positiveText("SETTINGS")
@@ -37,11 +37,11 @@ public class Permissions extends Activity {
                         final Intent i = new Intent();
                         i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                         i.addCategory(Intent.CATEGORY_DEFAULT);
-                        i.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                        i.setData(Uri.parse("package:" + mContext.getApplicationContext().getPackageName()));
                         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                         i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                        startActivity(i);
+                        mContext.startActivity(i);
                     }
                 })
                 .onNegative(new MaterialDialog.SingleButtonCallback() {
@@ -56,7 +56,7 @@ public class Permissions extends Activity {
 
     public void showDialogOK() {
         MaterialDialog dialog;
-        MaterialDialog.Builder builder = new MaterialDialog.Builder(this);
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(mContext);
         builder.title("Permission needed")
                 .content("This permission is required to access the weather data of your location.")
                 .positiveText("OK")
@@ -75,7 +75,7 @@ public class Permissions extends Activity {
         // permission is denied (this is the first time, when "never ask again" is not checked) so ask again explaining the usage of permission
         // shouldShowRequestPermissionRationale will return true
         //show the dialog or snackbar saying its necessary and try again otherwise proceed with setup.
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+        if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) mContext,
                 Manifest.permission.ACCESS_COARSE_LOCATION)) {
             showDialogOK();
         } //permission is denied (and never ask again is  checked)
