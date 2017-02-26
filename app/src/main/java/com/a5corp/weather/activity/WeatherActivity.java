45c72d27243f277c1c3f9b96f4d5cf69120f63cc
Aaditya Menon <AadityaMenon007@gmail.com>
2017-02-26 20:06:45 +0530

    Working Drawer Hamburger

--- app/src/main/java/com/a5corp/weather/activity/WeatherActivity.java
+++ app/src/main/java/com/a5corp/weather/activity/WeatherActivity.java
@@ -24,7 +24,12 @@ import com.a5corp.weather.permissions.GPSTracker;
 import com.a5corp.weather.permissions.Permissions;
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class WeatherActivity extends AppCompatActivity {
@@ -54,7 +59,35 @@ public class WeatherActivity extends AppCompatActivity {
                 fabClick();
             }
         });
-        new DrawerBuilder().withActivity(this).build();
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName(R.string.drawer_item_home);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.drawer_item_settings);
+//create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        boolean flag = false;
+                        switch (position) {
+                            case 1 : break;
+                            case 2 : startActivity(new Intent(WeatherActivity.this , AboutActivity.class));
+                                flag = true;
+                                break;
+                        }
+                        if (flag)
+                            return true;
+                        return false;
+                    }
+                })
+                .build();
+
     }
 
     @Override
