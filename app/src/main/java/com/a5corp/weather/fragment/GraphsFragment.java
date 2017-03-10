67998b8afb1f5e53747433f94e619075a26f7eba
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-11 01:04:51 +0530

    Basic Chart View

--- app/src/main/java/com/a5corp/weather/fragment/GraphsFragment.java
+++ app/src/main/java/com/a5corp/weather/fragment/GraphsFragment.java
@@ -13,6 +13,10 @@ import com.a5corp.weather.R;
 import com.a5corp.weather.internet.FetchWeather;
 import com.a5corp.weather.preferences.Preferences;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -21,8 +25,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 public class GraphsFragment extends Fragment {
@@ -46,6 +52,7 @@ public class GraphsFragment extends Fragment {
         super.onCreate(savedInstanceState);
         fw = new FetchWeather(getContext());
         pf = new Preferences(getContext());
+        setHasOptionsMenu(false);
     }
 
     @Override
@@ -74,7 +81,6 @@ public class GraphsFragment extends Fragment {
                 str = new JSONObject(bundle.getString("json", null));
                 list = str.getJSONArray("list");
                 for (int i = 0; i < 10; ++i) {
-                    //SimpleDateFormat simpleDateformat = new SimpleDateFormat("E" , Locale.US);
                     long day = list.getJSONObject(i).getLong("dt");
                     long temp = list.getJSONObject(i).getJSONObject("temp").getLong("day");
                     entries.add(new Entry(day , temp));
@@ -90,12 +96,27 @@ public class GraphsFragment extends Fragment {
     }
 
     public void loadChart() {
-        dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet = new LineDataSet(entries, "Temperature"); // add entries to dataset
         dataSet.setColor(Color.parseColor("#FF0000"));
         dataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
         LineData lineData = new LineData(dataSet);
         chart.setData(lineData);
+        Description desc = new Description();
+        desc.setText("Temperature, " + getString(R.string.c));
+        chart.setDescription(desc);
         chart.setBackgroundColor(Color.parseColor("#FFFFFF"));
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setDrawGridLines(false);
+
+        YAxis yAxisRight = chart.getAxisRight();
+        yAxisRight.setDrawGridLines(false);
+        yAxisRight.setDrawAxisLine(false);
+        yAxisRight.setDrawLabels(false);
+        yAxisRight.enableAxisLineDashedLine(2f , 1f , 2f);
+
         chart.invalidate();
     }
 }
