269fb483e1ff9c0cf67917e5b79667657e422e1b
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-15 01:25:41 +0530

    Moved to Previous BottomBar version

--- app/src/main/java/com/a5corp/weather/fragment/MapsFragment.java
+++ app/src/main/java/com/a5corp/weather/fragment/MapsFragment.java
@@ -2,7 +2,6 @@ package com.a5corp.weather.fragment;
 
 
 import android.os.Bundle;
-import android.support.annotation.FloatRange;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,16 +14,12 @@ import com.a5corp.weather.R;
 import com.a5corp.weather.activity.WeatherActivity;
 import com.a5corp.weather.preferences.Preferences;
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class MapsFragment extends Fragment {
 
     public View rootView;
     WebView webView;
-    Bundle bundle;
     private BottomBar mBottomBar;
     Preferences prefs;
 
@@ -39,37 +34,33 @@ public class MapsFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_maps, container, false);
         webView = (WebView) rootView.findViewById(R.id.webView);
         prefs = new Preferences(getContext());
-        mBottomBar = (BottomBar) rootView.findViewById(R.id.bottomBar);
-        bundle = this.getArguments();
-        ((WeatherActivity) getActivity()).hideFab();
-        mapsloader();
-        return rootView;
-    }
-
-    public void mapsloader() {
         webView.getSettings().setJavaScriptEnabled(true);
-        Log.i("Plain" , Float.toString(prefs.getLatitude()));
-        Log.i("Plain" , Float.toString(prefs.getLongitude()));
         webView.loadUrl("file:///android_asset/map.html?lat=" + prefs.getLatitude() + "&lon=" + prefs.getLongitude() + "&appid=" + getString(R.string.open_weather_maps_app_id));
-        mBottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+
+        mBottomBar = BottomBar.attach(getActivity() , savedInstanceState);
+        mBottomBar.setItems(R.menu.menu_bottombar);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
             @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.map_rain) {
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.map_rain) {
                     webView.loadUrl("javascript:map.removeLayer(windLayer);map.removeLayer(tempLayer);map.addLayer(rainLayer);");
-                }
-                else if (tabId == R.id.map_temperature) {
+                } else if (menuItemId == R.id.map_wind) {
+                    webView.loadUrl("javascript:map.removeLayer(rainLayer);map.removeLayer(tempLayer);map.addLayer(windLayer);");
+                } else if (menuItemId == R.id.map_temperature) {
                     webView.loadUrl("javascript:map.removeLayer(windLayer);map.removeLayer(rainLayer);map.addLayer(tempLayer);");
                 }
-                else if (tabId == R.id.map_wind) {
-                    webView.loadUrl("javascript:map.removeLayer(rainLayer);map.removeLayer(tempLayer);map.addLayer(windLayer);");
-                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
             }
         });
+        ((WeatherActivity) getActivity()).hideFab();
+        return rootView;
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mBottomBar.onSaveInstanceState();
     }
 }
