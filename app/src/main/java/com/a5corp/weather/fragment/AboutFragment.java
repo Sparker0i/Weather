729dacea8dd8e752cd458380b033adaaa6b64ed2
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-17 03:35:05 +0530

    Vibrator set for Card long click

--- app/src/main/java/com/a5corp/weather/fragment/AboutFragment.java
+++ app/src/main/java/com/a5corp/weather/fragment/AboutFragment.java
@@ -1,14 +1,15 @@
 package com.a5corp.weather.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,17 +17,9 @@ import android.widget.TextView;
 
 import com.a5corp.weather.BuildConfig;
 import com.a5corp.weather.R;
-import com.a5corp.weather.utils.CardInfo;
-
-import java.util.ArrayList;
 
 import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.recyclerview.internal.CardArrayRecyclerViewAdapter;
-import it.gmariotti.cardslib.library.recyclerview.view.CardRecyclerView;
 import it.gmariotti.cardslib.library.view.CardViewNative;
-import it.gmariotti.cardslib.library.view.base.CardViewWrapper;
-import it.gmariotti.cardslib.library.view.component.CardThumbnailView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -35,7 +28,6 @@ public class AboutFragment extends Fragment {
 
     CardViewNative[] cvn;
     View rootView;
-    Card card;
     String[] name = {"Open Weather Map" , "Weather Icons" , "Material Drawer" , "Material Dialogs" , "SystemBarTint" , "AndroidIconics" , "MPAndroidChart" , "Material Icons" , "Bottom Bar" , "CardsLib"};
     String[] creator = {"" , "Erik Flowers" , "Mike Penz" , "Aidan Follestad" , "Jeff Gilfelt" , "Mike Penz" , "Philipp Jahoda" , "Google" , "Iiro Krankka" , "Gabriele Mariotti"};
     String[] description = {"All the Weather Data You See in this app is due to the data being provided by Open Weather Map. You must be thanking them first" ,
@@ -48,6 +40,42 @@ public class AboutFragment extends Fragment {
             "Material design icons are the official icon set from Google that are designed under the material design guidelines.",
             "An easy to use library for implementing a Bottom Bar, just like the one you see in Google Photos app",
             "Card Library provides an easy way to display a UI Card using the Official Google CardView in your Android app."
+    };
+    String[] version = {
+            "2.5",
+            "2.0",
+            "5.8.1",
+            "0.9.2.3",
+            "1.0.3",
+            "2.8.2",
+            "3.0.1",
+            "2.2.0",
+            "2.1.1",
+            "2.1.0"
+    };
+    String[] license = {
+            "CC BY SA-3.0",
+            "CC BY SA-3.0",
+            "Apache-2.0",
+            "MIT",
+            "Apache-2.0",
+            "Apache-2.0",
+            "",
+            "Apache-2.0",
+            "Apache-2.0",
+            ""
+    };
+    String[] url = {
+            "http://openweathermap.org/",
+            "https://erikflowers.github.io/weather-icons/",
+            "http://mikepenz.github.io/MaterialDrawer/",
+            "https://github.com/afollestad/material-dialogs",
+            "https://github.com/jgilfelt/SystemBarTint",
+            "http://mikepenz.github.io/Android-Iconics",
+            "https://github.com/PhilJay/MPAndroidChart/",
+            "http://material.io/icons/",
+            "https://github.com/roughike/BottomBar",
+            "https://github.com/gabrielemariotti/cardslib"
     };
 
     public AboutFragment() {
@@ -81,6 +109,8 @@ public class AboutFragment extends Fragment {
                 int resID = getResources().getIdentifier(f, "id", getContext().getPackageName());
                 cvn[i] = (CardViewNative) rootView.findViewById(resID);
                 cvn[i].setBackgroundColor(Color.WHITE);
+                cvn[i].setRadius(20);
+                Log.i("Radius" , Float.toString(cvn[i].getRadius()));
                 String libraryName = name[i];
                 TextView libName = (TextView) cvn[i].findViewById(R.id.libraryName);
                 libName.setTextColor(Color.BLACK);
@@ -91,6 +121,34 @@ public class AboutFragment extends Fragment {
                 String libDescription = description[i];
                 TextView libDesc = (TextView) cvn[i].findViewById(R.id.libraryDescription);
                 libDesc.setText(libDescription);
+                String libVersion = version[i];
+                TextView libVer = (TextView) cvn[i].findViewById(R.id.libraryVersion);
+                libVer.setText(libVersion);
+                String libLicense = license[i];
+                TextView libLic = (TextView) cvn[i].findViewById(R.id.libraryLicense);
+                libLic.setText(libLicense);
+                final int j = i;
+                cvn[i].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String uri = url[j];
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(uri));
+                        startActivity(intent);
+                    }
+                });
+            cvn[i].setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    String uri = url[j];
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(uri));
+                    startActivity(intent);
+                    Vibrator vz = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
+                    vz.vibrate(25);
+                    return false;
+                }
+            });
         }
     }
 }
