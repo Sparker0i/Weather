33b68f03b271d62c3f1a3a710cf6fd0c7f452f9d
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-19 03:28:22 +0530

    Set with the Bottom Sheet Icons

--- app/src/main/java/com/a5corp/weather/fragment/CustomBottomSheetDialogFragment.java
+++ app/src/main/java/com/a5corp/weather/fragment/CustomBottomSheetDialogFragment.java
@@ -1,21 +1,77 @@
 package com.a5corp.weather.fragment;
 
 import android.app.Dialog;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.CoordinatorLayout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.a5corp.weather.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
+
+    TextView windIcon , rainIcon , snowIcon , humidityIcon , pressureIcon;
+    TextView windText , rainText , snowText , humidityText , pressureText;
+    TextView nightValue , mornValue , dayValue , eveValue;
+    TextView condition;
+    View rootView;
+    Bundle bundle;
+    Typeface weatherFont;
+    JSONObject json;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        bundle = this.getArguments();
+        try {
+            json = new JSONObject(bundle.getString("json"));
+        }
+        catch (JSONException ex) {
+            ex.printStackTrace();
+        }
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.dialog_modal , container, false);
+        condition = (TextView) rootView.findViewById(R.id.description);
+        nightValue = (TextView) rootView.findViewById(R.id.night_temperature);
+        mornValue = (TextView) rootView.findViewById(R.id.morning_temperature);
+        dayValue = (TextView) rootView.findViewById(R.id.day_temperature);
+        eveValue = (TextView) rootView.findViewById(R.id.evening_temperature);
+        windIcon = (TextView) rootView.findViewById(R.id.wind_icon);
+        windIcon.setTypeface(weatherFont);
+        windIcon.setText(getString(R.string.speed_icon));
+        rainIcon = (TextView) rootView.findViewById(R.id.rain_icon);
+        rainIcon.setTypeface(weatherFont);
+        rainIcon.setText(getString(R.string.rain));
+        snowIcon = (TextView) rootView.findViewById(R.id.snow_icon);
+        snowIcon.setTypeface(weatherFont);
+        snowIcon.setText(getString(R.string.snow));
+        humidityIcon = (TextView) rootView.findViewById(R.id.humidity_icon);
+        humidityIcon.setTypeface(weatherFont);
+        humidityIcon.setText(getString(R.string.humidity_icon));
+        pressureIcon = (TextView) rootView.findViewById(R.id.pressure_icon);
+        pressureIcon.setTypeface(weatherFont);
+        pressureIcon.setText(getString(R.string.pressure_icon));
+        windText = (TextView) rootView.findViewById(R.id.wind);
+        rainText = (TextView) rootView.findViewById(R.id.rain);
+        snowText = (TextView) rootView.findViewById(R.id.snow);
+        humidityText = (TextView) rootView.findViewById(R.id.humidity);
+        pressureText = (TextView) rootView.findViewById(R.id.pressure);
+        return rootView;
     }
 
     private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
@@ -42,6 +98,7 @@ public class CustomBottomSheetDialogFragment extends BottomSheetDialogFragment {
         CoordinatorLayout.Behavior behavior = layoutParams.getBehavior();
         if (behavior != null && behavior instanceof BottomSheetBehavior) {
             ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+
         }
     }
 }
