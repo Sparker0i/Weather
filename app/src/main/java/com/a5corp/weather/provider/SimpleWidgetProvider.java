80165db2da423ddcdda33d96bf1f4126cc466661
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-21 18:42:26 +0530

    Working Sample Widget

---
+++ app/src/main/java/com/a5corp/weather/provider/SimpleWidgetProvider.java
@@ -0,0 +1,36 @@
+package com.a5corp.weather.provider;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.a5corp.weather.R;
+
+import java.util.Locale;
+import java.util.Random;
+
+public class SimpleWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        for (int widgetId : appWidgetIds) {
+            String number = String.format(Locale.ENGLISH , "%03d", (new Random().nextInt(900) + 100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.simple_widget);
+            remoteViews.setTextViewText(R.id.textView, number);
+
+            Intent intent = new Intent(context, SimpleWidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
