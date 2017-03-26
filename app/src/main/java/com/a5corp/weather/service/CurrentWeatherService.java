536af124ffaef10758d4d5feca68a8163eeed0b3
Aaditya Menon <AadityaMenon007@gmail.com>
2017-03-26 15:42:14 +0530

    Changed to ELAPSEDE_WAKEUP from RTC in AlarmManager

--- app/src/main/java/com/a5corp/weather/service/CurrentWeatherService.java
+++ app/src/main/java/com/a5corp/weather/service/CurrentWeatherService.java
@@ -5,6 +5,7 @@ import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 
 import com.a5corp.weather.preferences.Preferences;
 import com.a5corp.weather.receiver.MyReceiver;
@@ -24,23 +25,19 @@ public class CurrentWeatherService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         preferences = new Preferences(this);
-        Calendar calendar = Calendar.getInstance();
-
-        calendar.set(Calendar.MONTH, 6);
-        calendar.set(Calendar.YEAR, 2013);
-        calendar.set(Calendar.DAY_OF_MONTH, 13);
-
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
-        calendar.set(Calendar.MINUTE, 48);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.AM_PM,Calendar.PM);
 
         Intent myIntent = new Intent(this, MyReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent,0);
 
         AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-        long recurring = (60 * 60000);  // in milliseconds
         if (preferences.getNotifs())
-            am.setRepeating(AlarmManager.RTC, Calendar.getInstance().getTimeInMillis(), recurring, pendingIntent);
+            am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HOUR,
+                    AlarmManager.INTERVAL_HOUR,
+                    pendingIntent);
+        else {
+            am.cancel(pendingIntent);
+            pendingIntent.cancel();
+        }
     }
 }
