5f05f4c60d2778338cfb965e440cf478d8a5c102
Aaditya Menon <AadityaMenon007@gmail.com>
2017-05-18 00:09:56 +0530

    Fix Daily View Scroll triggering SwipeRefresh

---
+++ app/src/main/java/com/a5corp/weather/widget/OnlyVerticalSwipeRefreshLayout.java
@@ -0,0 +1,38 @@
+package com.a5corp.weather.widget;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+public class OnlyVerticalSwipeRefreshLayout extends SwipeRefreshLayout {
+
+    private int touchSlop;
+    private float prevX;
+    private boolean declined;
+
+    public OnlyVerticalSwipeRefreshLayout(Context context, AttributeSet attrs) {
+        super( context, attrs );
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent( MotionEvent event ) {
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                prevX = MotionEvent.obtain( event ).getX();
+                declined = false; // New action
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float eventX = event.getX();
+                float xDiff = Math.abs( eventX - prevX );
+                if( declined || xDiff > touchSlop ){
+                    declined = true; // Memorize
+                    return false;
+                }
+                break;
+        }
+        return super.onInterceptTouchEvent( event );
+    }
+}
