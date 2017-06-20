a2d4ed63447ae4a1b36697fecea0512954a12268
Aaditya Menon <AadityaMenon007@gmail.com>
2017-06-20 19:41:17 +0530

    Removed Mail FAB from Paytm Activity

--- app/src/main/java/com/a5corp/weather/activity/PaytmDonateActivity.java
+++ app/src/main/java/com/a5corp/weather/activity/PaytmDonateActivity.java
@@ -18,15 +18,6 @@ public class PaytmDonateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_paytm_donate);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         if (getSupportActionBar() != null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
