c733bc3a4c92d8f48c9c882be8b106b6158bc61a
Aaditya Menon <aaditya@sparker0i.me>
2018-09-01 01:38:17 +0530

    Fixes #42

--- app/src/main/java/com/a5corp/weather/GlobalActivity.java
+++ app/src/main/java/com/a5corp/weather/GlobalActivity.java
@@ -1,6 +1,7 @@
 package com.a5corp.weather;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import com.a5corp.weather.model.Log;
@@ -20,6 +21,7 @@ public class GlobalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Log.i("Loaded" , "Global");
     }
 
