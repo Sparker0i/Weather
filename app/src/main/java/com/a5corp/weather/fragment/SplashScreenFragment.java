a1802412d05b486b98dbb233199df42c31e1a969
ayush@251196 <git2511ayush@gmail.com>
2018-12-12 23:15:37 +0530

    Added Splash screen

--- app/src/main/java/com/a5corp/weather/fragment/SplashScreenFragment.java
+++ app/src/main/java/com/a5corp/weather/fragment/SplashScreenFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
